084b78c27c8ec9eb11cdbc542af26393f1740f7f
Julien BRAT <bratjulien@gmail.com>
2017-01-30 13:29:43 +0100

    add icon page research

--- www/js/controllers/ResearchCtrl.js
+++ www/js/controllers/ResearchCtrl.js
@@ -1,95 +1,78 @@
 angular.module("PoV")
-  .controller('ResearchCtrl', function($scope) {
+  .controller('ResearchCtrl', function($scope, $cordovaGeolocation) {
+    $scope.showResearhForm = true;
+
     $scope.search = {
       departure: '',
       arrival: '',
       time: '',
       clear: '',
-      distance : ''
+      distance: '',
+      transport: ''
     };
 
     $scope.research = function() {
+      $scope.showResearhForm = false;
 
       if($scope.search.clear) {
         $scope.search.arrival = $scope.search.departure;
       }
 
+      $http('http://maps.googleapis.com/maps/api/geocode/json?address=' + $scope.departure + '&sensor=true')
+        .success(function(datas) {
+
+          var lat = datas[0].geometry.location.lat;
+          var long = datas[0].geometry.location.lng;
+
+
+
+
+
+        })
+        .error(function(error){
+
+        });
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: {lat: 45.7772220, lng: 3.0870250}
+      });
+
 
 
     }
+
     $scope.geolocCity = function() {
-                var posOptions = {timeout: 10000, enableHighAccuracy: false};
-                $cordovaGeolocation
-                    .getCurrentPosition(posOptions)
-                    .then(function (position) {
-                        console.log(position);
-                        var lat  = position.coords.latitude;
-                        var long = position.coords.longitude;
 
-                        $http({method: 'GET', url: 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+long+'&sensor=true'}).
-                        success(function(data, status, headers, config) {
-                            var ville = '';
-                            var pays = '';
-                            var compteur = 0;
+      var posOptions = {
+        timeout: 10000,
+        enableHighAccuracy: false
+      };
 
-                            console.log(data);
-                            angular.forEach(data.results[0].address_components, function(object) {
-                                if(compteur==2){
-                                    ville = object.long_name;
-                                }
-                                if(compteur==5){
-                                    pays = object.long_name;
-                                }
-                                compteur = compteur + 1;
-                            });
-                            $scope.search.depart = ville+","+pays;
+      $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(function (position) {
+          var lat  = position.coords.latitude;
+          var long = position.coords.longitude;
 
-                        }).
-                        error(function(data, status, headers, config) {
-                            alert("Une erreur est survenue lors de la tentative de géolocalisation")
-                        });
+          $http(
+            {
+              method: 'GET',
+              url: 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+ lat +','+ long +'&sensor=true'
+            }
+          )
+          .success(function(data) {
+            $scope.search.depart = data[0].formatted_address;
+          })
+          .error(function(error) {
+            $scope.errorMessage = "Une erreur est survenue lors de la tentative de géolocalisation";
+          });
+        }, function(err) {
+          $scope.errorMessage = "Erreur lors de la récupération de votre position actuelle";
+        });
+    };
 
-                    }, function(err) {
-                        alert("Erreur lors de la récupération de votre position actuelle");
-                    });
+    $scope.setTransport = function() {
 
-            };
-
-            //--------------- Geolocalisation City start -------------------------//
-            $scope.geolocCity = function() {
-                var posOptions = {timeout: 10000, enableHighAccuracy: false};
-                $cordovaGeolocation
-                    .getCurrentPosition(posOptions)
-                    .then(function (position) {
-                        var lat  = position.coords.latitude;
-                        var long = position.coords.longitude;
-
-                        $http({method: 'GET', url: 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+long+'&sensor=true'}).
-                        success(function(data, status, headers, config) {
-                            var ville = '';
-                            var pays = '';
-                            var compteur = 0;
-
-                            console.log(data);
-                            angular.forEach(data.results[0].address_components, function(object) {
-                                if(compteur==2){
-                                    ville = object.long_name;
-                                }
-                                if(compteur==5){
-                                    pays = object.long_name;
-                                }
-                                compteur = compteur + 1;
-                            });
-                            $scope.search.depart = ville+","+pays;
-
-                        }).
-                        error(function(data, status, headers, config) {
-                            alert("Une erreur est survenue lors de la tentative de géolocalisation")
-                        });
-
-                    }, function(err) {
-                        alert("Erreur lors de la récupération de votre position actuelle");
-                    });
-            };
-
+    }
   });

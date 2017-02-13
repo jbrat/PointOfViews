db56c1188a304a58a96210b117e7f698b2b2df48
TRAGLOM <traglom@gmail.com>
2017-02-13 09:55:24 +0100

    retour a une version plus stable

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,16 +3,26 @@ angular.module('PoV.controllers', [])
   .controller('AppCtrl', function($scope, $state, $ionicModal, $timeout, $ionicHistory, user, FirebaseInstance) {
     $scope.user = user;
 
+    if(user.isLogin)
+    {
+      console.log("utilisateur log");
+    }
+    else
+    {
+      console.log("utilisateur pas log");
+    }
     $scope.disconnect = function() {
 
       FirebaseInstance.auth().signOut().then(function(error) {
         user.isLogin = false;
         user.email = "";
 
-        $ionicHistory.clearCache().then(function() {
+        $ionicHistory.clearCache().then(function(user) {
           //now you can clear history or goto another state if you need
           $ionicHistory.clearHistory();
           $ionicHistory.nextViewOptions({ disableBack: true, historyRoot: true });
+
+
 
           $state.go('app.home', {reload: true});
         })

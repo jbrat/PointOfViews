1786fc0bbcfe8bacefde2a53edeed9ade5d05e95
Sangfer <alexis.barthelemy02@gmail.com>
2017-02-14 15:32:27 +0100

    Get all data from 4square

--- www/js/controllers/SearchCtrl.js
+++ www/js/controllers/SearchCtrl.js
@@ -8,44 +8,61 @@ angular.module("PoV")
       word: ''
     };
 
+  $scope.printForm = true;
+  var result4quare=[];
+
   $scope.searchInformations = function(){
-
+  $scope.printForm = false;
       // load filter options from localStorage
-      records = JSON.parse(localStorage.getItem('records'));
-      if (records == null) {
-        records = {
-          food: true,
-          shops: false,
-          outdoors: false
-        };
-        localStorage.setItem('records', JSON.stringify(records));
-      }
 
-      // create the category array
-      var category= [];
-      if (records.food) category.push('food');
-      if (records.shops) category.push('shops');
-      if (records.outdoors) category.push('outdoors');
 
       $http.get(
         "https://api.foursquare.com/v2/venues/explore/?near=" +
         $scope.search.word  +
-        "&venuePhotos=1&section=" +
-        category.join(',') +
+        "&venuePhotos=1&section=food,shops,outdoors"+
         "&client_id=" + "HIVPXSYJDK1FLFSSJHCREMFOO34OAFQPGRIWT42EFMLQXW03" +
         "&client_secret=" + "W3P4YQZKFSCUFKUOFAVJFF0XMD1AXTCFUG0UFOQGCNPFMHF1" +
         " &v=20131124"
       )
       .success(function(data){
 
-      $scope.result= angular.fromJson(data);
+           var result4quareCoord=[];
+           var result4quareContact=[];
 
-                    console.log("Sucess");
-                    console.log($scope.result.response.geocode.displayString);
-                    console.log()
-                    }
+             var result= angular.fromJson(data);
+            console.log("Sucess");
+
+            //Name
+            result4quare['name']= result.response.groups[0].items[0].venue.name;
+            console.log(result4quare['name']);
+
+            // GPS coordinates
+            result4quareCoord['lat']=result.response.geocode.center.lat;
+            result4quareCoord['lng']=result.response.geocode.center.lng;
+            result4quare['coord']= result4quareCoord;
+
+            // Contact
+            if ( result.response.groups[0].items[0].venue.contact.phone ? result4quareContact['phone']=result.response.groups[0].items[0].venue.contact.phone : null);
+            if ( result.response.groups[0].items[0].venue.contact.twitter ? result4quareContact['twitter']=result.response.groups[0].items[0].venue.contact.twitter : null);
+            if ( result.response.groups[0].items[0].venue.contact.instagram ? result4quareContact['instagram']=result.response.groups[0].items[0].venue.contact.instagram :  null);
+            if ( result.response.groups[0].items[0].venue.contact.facebookName ? result4quareContact['facebook']= result.response.groups[0].items[0].venue.contact.facebookName : null)
+            result4quare['contact']=result4quareContact;
+
+            //Location
+            result4quare['location']=result.response.groups[0].items[0].venue.location.formattedAddress;
+
+            //URL
+            if ( result.response.groups[0].items[0].venue.url ? result4quare['url']=result.response.groups[0].items[0].venue.url : null);
+
+            //Rating
+            if ( result.response.groups[0].items[0].venue.rating ? result4quare['rating']=result.response.groups[0].items[0].venue.rating : null);
+
+            //Categories
+            result4quare['categorie']=result.response.groups[0].items[0].venue.categories[0].name;
+
+            }
        ).error(function(data) {
-                     console.log("Error");
+                     console.log("Unknown place, try again");
                     })
     };
   });
